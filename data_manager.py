36477721178545fe679b984592c60ab498906e09
Edwin Zwanenburg <edwinzwanenburg@Macs-MacBook-Pro.local>
2021-05-05 15:39:37 -0500

    connecting with sheety api

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,25 @@
+import requests
+from pprint import pprint
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self, city, code, price):
+        self.city = city
+        self.code = code
+        self.price = price
+
+    def post_data(self):
+        data = {
+            "price":{
+                "city": self.city,
+                "iataCode": self.code,
+                "lowestPrice": self.price               
+            }
+        }
+        sheet_res = requests.post(url="https://api.sheety.co/cb662cbd335c18760ecd9cb30b9d13b1/flightDeals/prices", json=data)
+        print(sheet_res.json())
+
+    def get_data(self):
+        response = requests.get("https://api.sheety.co/cb662cbd335c18760ecd9cb30b9d13b1/flightDeals/prices")
+        response.raise_for_status()
+        return response.json()["prices"]
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from flight_data import FlightData
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+
+
+manager = DataManager("Cancun", "CUN", "300")
+f_search = FlightSearch()
+
+sheet_data = manager.get_data()
+
+cities = []
+for data in sheet_data:
+    if data["iataCode"] == "":
+        cities.append(data["city"])
+
+f_search.cities = cities
+
+print(f_search.cities)
